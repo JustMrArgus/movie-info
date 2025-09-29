886d297f9bd2eb5e93d3a91774085b3e61697340
JustMrArgus <argusprogramming@protonmail.com>
2025-09-30 02:42:58 +0300

    Create server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = require("./app.js");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}... `);
+});
