04cf5a145b39db10c02cd47b203a54739c18bb16
JustMrArgus <argusprogramming@protonmail.com>
2025-09-30 03:11:01 +0300

    Add Movie model

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,35 @@
-const sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const path = require("path");
 
-const sequelize = new sequelize.Sequelize({
+const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: path.join(__dirname, "../db/db.sqlite"),
   logging: false,
 });
+
+const Movie = sequelize.define("Movie", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  format: {
+    type: DataTypes.ENUM("VHS", "DVD", "Blu-ray"),
+    allowNull: false,
+  },
+  actors: {
+    type: DataTypes.JSON,
+    allowNull: false,
+  },
+});
+
+module.exports = { sequelize, Movie };
