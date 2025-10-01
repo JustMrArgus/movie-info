be79ba924b4846b1f555f446fe7d20c0f859eaf3
JustMrArgus <argusprogramming@protonmail.com>
2025-10-01 14:33:24 +0300

    Create routes for movie controller

---
+++ backend/routes/movie.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const movieController = require("../controllers/movie.controller");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(movieController.createMovie)
+  .get(movieController.getMovies);
+
+router.route("/import").post(movieController.createManyMovies);
+
+router
+  .route("/:id")
+  .get(movieController.getMovieById)
+  .patch(movieController.updateMovie)
+  .delete(movieController.deleteMovie);
